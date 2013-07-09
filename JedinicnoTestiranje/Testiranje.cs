11f78d905245b1baae67f35ff4b51561e28455fe
Franjo Pustički <franjo.pusticki@gmail.com>
2013-07-09 22:59:17 +0200

    Dodano testiranje aplikacije

---
+++ JedinicnoTestiranje/Testiranje.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aplikacija;
+
+namespace JedinicnoTestiranje
+{
+    [TestClass]
+    public class Testiranje
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            frmPrijava dbClass = new frmPrijava();
+            string UserName = "ivo";
+            string Password = "ivic";
+            string s = dbClass.txtIme;
+
+            Assert.AreEqual(true, s);
+
+            string UserName2 = "skjdsd";
+            string Password2 = "123456";
+            string p = UserName2;
+
+            Assert.AreEqual(false, p);
+        }
+    }
+}
